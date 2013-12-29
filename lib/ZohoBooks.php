cfa9b8a5bb07d50e433e4286b19ee741fcedd0d3
Ransom Carroll <ransom.carroll@cartdesigners.com>
2013-12-29 14:43:53 -0600

    PSR-0

---
+++ lib/ZohoBooks.php
@@ -0,0 +1,14 @@
+<?php
+
+// Tested on PHP 5.2, 5.3
+
+// This snippet (and some of the curl code) due to the Facebook SDK.
+if (!function_exists('curl_init')) {
+  throw new Exception('ZohoBooks needs the CURL PHP extension.');
+}
+if (!function_exists('json_decode')) {
+  throw new Exception('ZohoBooks needs the JSON PHP extension.');
+}
+
+// Stripe singleton
+require(dirname(__FILE__) . '/ZohoBooks/ZohoBooks.php');
